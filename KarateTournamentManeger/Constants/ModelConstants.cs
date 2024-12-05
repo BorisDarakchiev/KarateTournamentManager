97e39af92d183c2cb7ff35ef2395f3260863b476
Boris Darakchiev <darakchievbg@gmail.com>
2024-12-05 22:26:13 +0200

    ModelConstants update

--- KarateTournamentManeger/Constants/ModelConstants.cs
+++ KarateTournamentManeger/Constants/ModelConstants.cs
@@ -12,17 +12,5 @@
         { "Final", "Финал" },
         { "RoundRobin", "Всеки срещу всеки" }
     };
-
-        public static readonly Dictionary<string, string> StageToName = new Dictionary<string, string>
-    {
-        { "Preliminary", "Предварителен етап" },
-        { "SixteenthFinal", "Шестнайсетинафинал" },
-        { "EighthFinal", "Осминафинал" },
-        { "QuarterFinal", "Четвъртфинал" },
-        { "SemiFinal", "Полуфинал" },
-        { "Final", "Финал" },
-        { "RoundRobin", "Всеки срещу всеки" }
-    };
-
     }
 }
